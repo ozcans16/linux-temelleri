1ff89410bc91fcf7738592b23ec2894765e960e8
Mehmet <mehmetozcan795@gmail.com>
2026-03-26 15:59:57 +0300

    hafta-6
    bilgilers

---
+++ linux-temelleri.py
@@ -0,0 +1,13 @@
+###Linux’ta 
+#ls dosyaları listeler,
+#cd dizin değiştirir,
+#grep metin arar,
+#mkdir klasör oluşturur,
+#chmod izinleri ayarlar ve top sistem süreçlerini gösterir;
+###paket yönetiminde 
+#apt,pacman ve dnf yazılım kurma/güncelleme/kaldırma için kullanılır;
+###dosya izinleri 
+#r (okuma),
+#w (yazma),
+#x (çalıştırma) olarak kullanıcı, grup ve diğerlerine atanır
+###servis yönetimi ise systemctl ile yapılır (başlatma, durdurma, durum kontrolü, otomatik başlatma).
